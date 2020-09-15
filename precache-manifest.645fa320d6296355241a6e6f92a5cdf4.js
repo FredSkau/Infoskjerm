0f77336d011b3ece24c585330c309a2667004b2d
Fredrik Skaug <fredskau@stud.ntnu.no>
2020-09-15 14:33:09 +0200

    Updates

---
+++ precache-manifest.645fa320d6296355241a6e6f92a5cdf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a30471b1162a1bccd0ab4ff10c3b11",
+    "url": "/Infoskjerm/index.html"
+  },
+  {
+    "revision": "48b7132ddd03a6d01748",
+    "url": "/Infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "866741c345cef7d7d1b0",
+    "url": "/Infoskjerm/static/js/2.e13d1c94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Infoskjerm/static/js/2.e13d1c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b7132ddd03a6d01748",
+    "url": "/Infoskjerm/static/js/main.baf2360d.chunk.js"
+  },
+  {
+    "revision": "ff7c7fb72a57287e703d",
+    "url": "/Infoskjerm/static/js/runtime-main.4fa9607a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Infoskjerm/static/media/logo.5d5d9eef.svg"
+  }
+]);
